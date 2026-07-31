c52b48abbad51574d89cb804378079e1c914191b
Yu Wang <70514121+wangyu-sd@users.noreply.github.com>
2026-07-31 15:43:20 +0800

    Make hypothesis test self contained

--- tests/test_proof_hypotheses.py
+++ tests/test_proof_hypotheses.py
@@ -5,7 +5,24 @@ from mechet.proof_hypotheses import (
     summarize_hypotheses,
     survival_curve,
 )
-from tests.test_proof_curriculum import substitution_proof
+from mechet.proof_program import ChargeAction, ProofEdge, ProofProgram, format_proof_output
+
+
+def substitution_proof() -> str:
+    return format_proof_output(
+        ProofProgram(
+            target_smiles="[CH3:1][OH:2]",
+            roots={"s0": ["[Br-:3]", "[Na+:4]"]},
+            precursor_state_id="s1",
+            edges=[ProofEdge(
+                "s0",
+                "s1",
+                bonds=[(1, 2, -1), (1, 3, +1)],
+                lone_pairs=[(2, +2), (3, -2)],
+                charges=[ChargeAction(2, 0, -1), ChargeAction(3, -1, 0)],
+            )],
+        )
+    )
 
 
 def test_hypothesis_scoring_uses_structural_endpoint_and_deduplicates():
